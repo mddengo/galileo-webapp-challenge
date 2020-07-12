c0e544abad99612874dd32887c6849cf0100b3c0
mddengo <mddengo@gmail.com>
2020-07-11 20:39:23 -0400

    add constants

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+/**
+ * Constants for the web app
+ */
+import { Action } from 'redux';
+import { RootState } from './redux';
+import { ThunkAction } from 'redux-thunk';
+export const GET_PROVIDERS_URL = 'https://testapi.io/api/akirayoglu/0/reference/getDoctors';
+export const GET_TASKS_URL = 'https://testapi.io/api/akirayoglu/0/tasks/getTasks';
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>
