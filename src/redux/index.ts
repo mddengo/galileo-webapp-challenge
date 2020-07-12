235aa859146da0f99b162b8f934b2c46b79cdf03
mddengo <mddengo@gmail.com>
2020-07-11 20:40:19 -0400

    configure store

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,6 +1,9 @@
+/**
+ * Define root reducer and root state for the store
+ */
 import { combineReducers } from 'redux';
-import {ProviderReducer} from './modules/provider/provider';
-import {TaskReducer} from './modules/task/task';
+import { ProviderReducer } from './modules/provider/provider';
+import { TaskReducer } from './modules/task/task';
 
 export const rootReducer = combineReducers({
   providers: ProviderReducer,
